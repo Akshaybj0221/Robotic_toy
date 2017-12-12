fef6d552749c839928d0433a4dec225d134d6d86
Akshay <akshaybj0221@gmail.com>
2017-12-11 23:39:52 -0800

    Changed topic to be subscribed

--- src/ControlTurtlebot.py
+++ src/ControlTurtlebot.py
@@ -10,7 +10,7 @@ class ControlTurtlebot():
 		
 		#Topic on which msg will be sent
 		#cmd_vel_mux/input/navi		
-		self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+		self.cmd_vel = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
 		rate = rospy.Rate(10);
 		move_cmd = Twist()
 		move_cmd.linear.x = 0.3
