7569281bba8fda13649a7ff316ec9bb210ec1cad
Akshay <akshaybj0221@gmail.com>
2017-12-12 00:13:07 -0800

    Added tentative logic to number of sides and maintaine the pause duration

--- src/ControlTurtlebot.py
+++ src/ControlTurtlebot.py
@@ -11,21 +11,50 @@ class ControlTurtlebot():
 		#Topic on which msg will be sent
 		#cmd_vel_mux/input/navi		
 		self.cmd_vel = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
-		rate = rospy.Rate(10);
-		move_cmd = Twist()
-
+	
+		rate = 50
+		r = rospy.Rate(rate);
+		
+		linear_speed = 0.2
+		goal_distance = 1.0 
+		linear_duration = goal_distance / linear_speed
+		angular_speed = 1.0
 		n = 4
 		angle = 360 / n		
 		pi = 3.14
 		rad = angle*(pi/180)
+		
+		angular_duration = rad / angular_speed
 
-		move_cmd.linear.x = 0.3
-		#rate.sleep(0.3)
+		
 		move_cmd.angular.z = rad
-		#rate.sleep(0.5)
-		while not rospy.is_shutdown():
+		move_cmd.linear.x = linear_speed
+		ticks = int(linear_duration * rate)
+
+		for t in range(ticks):
 			self.cmd_vel.publish(move_cmd)
-			rate.sleep()
+			r.sleep()
+
+		move_cmd = Twist()
+		self.cmd_vel.publish(move_cmd)
+		rate.sleep(1)
+		
+		move_cmd.angular.z = angular_speed
+		ticks = int(goal_angle * rate)
+
+		for t in range(ticks):
+			self.cmd_vel.publish(move_cmd)
+			r.sleep()
+
+		move_cmd = Twist()
+		self.cmd_vel.publish(move_cmd)
+		rate.sleep(1)	
+
+		self.cmd_vel.publish(Twist)
+
+#		while not rospy.is_shutdown():
+#			self.cmd_vel.publish(move_cmd)
+#			rate.sleep()
 
 	def shutdown(self):
 		rospy.loginfo("Stopping turtlebot")
