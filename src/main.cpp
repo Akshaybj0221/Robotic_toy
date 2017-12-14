d2388baee63198ff6966c44ac2f58390ed54cc61
Akshay <akshaybj0221@gmail.com>
2017-12-14 03:43:51 -0800

    Added class object in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "free_space_navigation_node");
 	ros::NodeHandle n;
 
+	ControlTurtlebot obj;
+
 	//subscribe to the odometry topic to get the position of the robot
-	pose_subscriber = n.subscribe("/odom", 10, poseCallback);
+	obj.pose_subscriber = n.subscribe("/odom", 10, poseCallback);
 	//register the velocity publisher
-	pub =n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1000);
+	obj.pub =n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1000);
 
 	ros::spinOnce();
 	ros::Rate loop(1);
@@ -23,7 +25,7 @@ int main(int argc, char **argv){
 
 		double velocity = 0.3;
 
-		moveShape(1.0, 3, 120, velocity);
+		obj.moveShape(1.0, 3, 120, velocity);
 		
 		//exercise: try to remove the ros::SpinOnce() and observe and comment the result
 		ros::spinOnce();loop.sleep();ros::spinOnce();
