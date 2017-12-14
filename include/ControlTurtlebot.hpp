6438156a4d93bc5f600a58a6c9222b16271608b6
Akshay <akshaybj0221@gmail.com>
2017-12-14 03:25:22 -0800

    Added functions and variables to the header file

--- include/ControlTurtlebot.hpp
+++ include/ControlTurtlebot.hpp
@@ -3,40 +3,46 @@
 #ifndef INCLUDE_CONTROLTURTLEBOT_HPP_
 #define INCLUDE_CONTROLTURTLEBOT_HPP_
 
-#include <iostream>
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
-#include "std_msgs/Float64.h"
-#include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
+#include <tf/transform_listener.h>
+#include <fstream>
 
 class ControlTurtlebot {
+  
+
+  private:
+   
+  #define ANGULAR_VELOCITY_MINIMUM_THRESHOLD 0.4
+  #define pi 22/7
+  
+  //declare publishers
+  ros::Publisher pub;
+  //declare subscribers
+  ros::Subscriber pose_subscriber;
+  //global variable to update the position of the robot
+  nav_msgs::Odometry turtlebot_odom_pose;
+
+
   public:
 
-    ros::Subscriber sub;
+  //callback function for the /odom topic to update the pose
+  void poseCallback(const nav_msgs::Odometry::ConstPtr & pose_message);
+  
+  //the function that makes the robot moves forward and backward
+  void move_linear(double speed, double distance, bool isForward);
+  
+  //the function that makes the robot rotates left and right
+  double rotate(double ang_vel, double angle_radian, bool isClockwise);
+  
+  double degree2radian(double degreeAngle);
+  double radian2degree(double radianAngle);
+  
+  void moveShape(double sideLength, double totalSides, double angle, double velocity);
 
-//    int i, t, ticks;    
-/*    int rate = 50;
-//    ros::Rate r = ros::Rate loop_rate(50);		
-
-    int n = 4;
-
-    float linear_speed = 0.5;
-    float goal_distance = 1.0;
-    float linear_duration = goal_distance / linear_speed;
-    float angular_speed = 1.0;
-    float angle = 360/n;	
-    float pi = 3.14;
-    float rad = angle*(pi/180);
-    float angular_duration = (rad / angular_speed);
-
-    geometry_msgs::Twist move_cmd;
-    ros::Publisher cmd_vel;
-*/
-    ControlTurtlebot(ros::NodeHandle);
-    ~ControlTurtlebot();
-    
-    int init(ros::NodeHandle);
-    int shutdown(ros::NodeHandle);
 };
 
 #endif
