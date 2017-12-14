f4281c09bb9a3d75913620eb557352b6af56de87
Akshay <akshaybj0221@gmail.com>
2017-12-13 23:53:12 -0800

    Reverted last changes in sleep method Sprint 2

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,7 @@ int main(int argc, char **argv){
 		ticks = int(linear_duration * rate);
 		for(int t=0; t<=ticks; t++){
 			cmd_vel.publish(move_cmd);
-			ros::Duration(1).sleep();
-
-//			r.sleep();
+			r.sleep();
 		}
 
 
