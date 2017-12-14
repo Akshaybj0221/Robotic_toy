bbb0b194db05240a6b77e8695e86d74da3c4783c
Akshay <akshaybj0221@gmail.com>
2017-12-14 03:29:04 -0800

    Added function definitions in ControlTurtlebot.cpp file along with new logic Sprin 2

--- src/ControlTurtlebot.cpp
+++ src/ControlTurtlebot.cpp
@@ -1,80 +1,155 @@
 
-#include "ControlTurtlebot.hpp"
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-ControlTurtlebot::ControlTurtlebot(ros::NodeHandle nh){
-  	
-  init(nh);
-  shutdown(nh);
 
+
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr & pose_message){
+	turtlebot_odom_pose.pose.pose.position.x=pose_message->pose.pose.position.x;
+	turtlebot_odom_pose.pose.pose.position.y=pose_message->pose.pose.position.y;
+	turtlebot_odom_pose.pose.pose.position.z=pose_message->pose.pose.position.z;
+
+	turtlebot_odom_pose.pose.pose.orientation.w=pose_message->pose.pose.orientation.w;
+	turtlebot_odom_pose.pose.pose.orientation.x=pose_message->pose.pose.orientation.x;
+	turtlebot_odom_pose.pose.pose.orientation.y=pose_message->pose.pose.orientation.y;
+	turtlebot_odom_pose.pose.pose.orientation.z=pose_message->pose.pose.orientation.z;
 }
 
-int ControlTurtlebot::init(ros::NodeHandle nh){
 
-    int i, t, ticks;    
-    int rate = 50;
-//    ros::Rate r = ros::Rate loop_rate(50);		
-
-    ros::Rate r(rate);
-
-    int n = 4;
-
-    float linear_speed = 0.5;
-    float goal_distance = 1.0;
-    float linear_duration = goal_distance / linear_speed;
-    float angular_speed = 1.0;
-    float angle = 360/n;	
-    float pi = 3.14;
-    float rad = angle*(pi/180);
-    float angular_duration = (rad / angular_speed);
+void moveShape(double sideLength, double totalSides, double angle, double velocity){
+	for (int i=0;i<totalSides;i++){
+		move_linear(velocity, sideLength, true);
+		rotate (velocity, degree2radian(angle), true);
+	}	
+}
 
 
-	for(int i=0;i<=n;i++){
-		geomtery_msgs::Twist move_cmd;
-		move_cmd.linear.x = linear_speed;
-		ROS_INFO("Inside 1st loop");
-		ticks = int(linear_duration * rate);
+void move_linear(double speed, double distance, bool isForward){
+	//delcare a Twist message to send velocity commands
+	geometry_msgs::Twist VelocityMessage;
+	//initial pose of the turtlebot before start moving
+	nav_msgs::Odometry initial_turtlebot_odom_pose;
 
-		for(int t=0; t<=ticks; t++){
-			cmd_vel.publish(move_cmd);
-			r.sleep();
+	//set the linear velocity to a positive value if isFoward is true
+	if (isForward)
+		VelocityMessage.linear.x =abs(speed);
+	//all velocities of other axes must be zero.
+	VelocityMessage.linear.y = VelocityMessage.linear.z =VelocityMessage.angular.x =VelocityMessage.angular.y =VelocityMessage.angular.z =0;
+
+	double distance_moved = 0.0;
+	ros::Rate loop_rate(10); // we publish the velocity at 10 Hz (10 times a second)
+
+	//we update the initial_turtlebot_odom_pose using the turtlebot_odom_pose global variable updated in the callback function poseCallback
+	initial_turtlebot_odom_pose = turtlebot_odom_pose;
+
+	do{
+		pub.publish(VelocityMessage);
+		ros::spinOnce();
+		loop_rate.sleep();
+		distance_moved = sqrt(pow((turtlebot_odom_pose.pose.pose.position.x-initial_turtlebot_odom_pose.pose.pose.position.x), 2) +
+				pow((turtlebot_odom_pose.pose.pose.position.y-initial_turtlebot_odom_pose.pose.pose.position.y), 2));
+
+	}while((distance_moved<distance)&&(ros::ok()));
+	//finally, stop the robot when the distance is moved
+	VelocityMessage.linear.x =0;
+	pub.publish(VelocityMessage);
+}
+
+
+
+double rotate(double angular_velocity, double radians,  bool clockwise)
+{
+
+	//delcare a Twist message to send velocity commands
+	geometry_msgs::Twist VelocityMessage;
+	//declare tf transform listener: this transform listener will be used to listen and capture the transformation between
+	// the odom frame (that represent the reference frame) and the base_footprint frame the represent moving frame
+	tf::TransformListener TFListener;
+	//declare tf transform
+	//init_transform: is the transformation before starting the motion
+	tf::StampedTransform init_transform;
+	//current_transformation: is the transformation while the robot is moving
+	tf::StampedTransform current_transform;
+	//initial coordinates (for method 3)
+	nav_msgs::Odometry initial_turtlebot_odom_pose;
+
+	double angle_turned =0.0;
+
+	//validate angular velocity; ANGULAR_VELOCITY_MINIMUM_THRESHOLD is the minimum allowed
+	angular_velocity=((angular_velocity>ANGULAR_VELOCITY_MINIMUM_THRESHOLD)?angular_velocity:ANGULAR_VELOCITY_MINIMUM_THRESHOLD);
+
+	while(radians < 0) radians += 2*pi;
+	while(radians > 2*pi) radians -= 2*pi;
+
+	//wait for the listener to get the first message
+	TFListener.waitForTransform("base_footprint", "odom", ros::Time(0), ros::Duration(1.0));
+
+	//record the starting transform from the odometry to the base frame
+	TFListener.lookupTransform("base_footprint", "odom", ros::Time(0), init_transform);
+
+	//the command will be to turn at 0.75 rad/s
+	VelocityMessage.linear.x = VelocityMessage.linear.y = 0.0;
+	VelocityMessage.angular.z = angular_velocity;
+	if (clockwise) VelocityMessage.angular.z = -VelocityMessage.angular.z;
+
+	//the axis we want to be rotating by
+	tf::Vector3 desired_turn_axis(0,0,1);
+	if (!clockwise) desired_turn_axis = -desired_turn_axis;
+
+	ros::Rate rate(10.0);
+	bool done = false;
+	while (!done )
+	{
+		//send the drive command
+		pub.publish(VelocityMessage);
+		rate.sleep();
+		//get the current transform
+		try
+		{
+			TFListener.waitForTransform("base_footprint", "odom", ros::Time(0), ros::Duration(1.0));
+			TFListener.lookupTransform("base_footprint", "odom", ros::Time(0), current_transform);
+		}
+		catch (tf::TransformException ex)
+		{
+			ROS_ERROR("%s",ex.what());
+			break;
+		}
+		tf::Transform relative_transform = init_transform.inverse() * current_transform;
+		tf::Vector3 actual_turn_axis = relative_transform.getRotation().getAxis();
+		angle_turned = relative_transform.getRotation().getAngle();
+
+		if (fabs(angle_turned) < 1.0e-2) continue;
+		if (actual_turn_axis.dot(desired_turn_axis ) < 0 )
+			angle_turned = 2 * pi - angle_turned;
+
+		if (!clockwise)
+			VelocityMessage.angular.z = (angular_velocity-ANGULAR_VELOCITY_MINIMUM_THRESHOLD) * (fabs(radian2degree(radians-angle_turned)/radian2degree(radians)))+ANGULAR_VELOCITY_MINIMUM_THRESHOLD;
+		else
+			if (clockwise)
+				VelocityMessage.angular.z = (-angular_velocity+ANGULAR_VELOCITY_MINIMUM_THRESHOLD) * (fabs(radian2degree(radians-angle_turned)/radian2degree(radians)))-ANGULAR_VELOCITY_MINIMUM_THRESHOLD;
+
+		if (angle_turned > radians) {
+			done = true;
+			VelocityMessage.linear.x = VelocityMessage.linear.y = VelocityMessage.angular.z = 0;
+			pub.publish(VelocityMessage);
 		}
 
-		geomtery_msgs::Twist move_cmd;
-		cmd_vel.publish(move_cmd);
-		ros::Duration(1).sleep();
-		
-		move_cmd.angular.z = angular_speed;
-		ticks = int(rad * rate);
-	
-		for(int t=0; t<=ticks; t++){
-			cmd_vel.publish(move_cmd);
-			r.sleep();
-		}	
-		geomtery_msgs::Twist move_cmd;
-		cmd_vel.publish(move_cmd);
-		ros::Duration(1).sleep();
 	}
-
-	cmd_vel.publish(move_cmd);	//instead if move_cmd it was "Twist()", so take care)
-	
-	while(ros::ok()){
-		cmd_vel.publish(move_cmd);
-		r.sleep();
-
-	return 0;
+	if (done) return angle_turned;
+	return angle_turned;
 }
 
-int ControlTurtlebot::shutdown(ros::NodeHandle nh){
-
-	ROS_INFO("Stopping Turtlebot");
-	cmd_vel.publish(geometry_msgs::Twist);
-	ros::Duration(1).sleep();	//Here i have used the replacement for the same Twist() in cpp
-
-	return 0;
+/* makes conversion from radian to degree */
+double radian2degree(double radianAngle){
+	return (radianAngle*57.2957795);
 }
 
-ControlTurtlebot::~ControlTurtlebot(){
+
+/* makes conversion from degree to radian */
+double degree2radian(double degreeAngle){
+	return (degreeAngle/57.2957795);
 }
+
+
+
+
 
 
